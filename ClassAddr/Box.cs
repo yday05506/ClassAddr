a759da0132d70f247f3be9559bc980a2f88f3460
yday05506 <yangdayeon2@gmail.com>
2023-05-25 09:21:00 +0900

    속성(Property) 실습

---
+++ ClassAddr/Box.cs
@@ -0,0 +1,6 @@
+﻿namespace ClassAddr
+{
+    internal class Box
+    {
+    }
+}
--- ClassAddr/Program.cs
+++ ClassAddr/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,7 +13,21 @@ namespace ClassAddr
         static void Main(string[] args)
         {
             // 소멸자 예시
-            Product product = new Product("과자", 1000);
+            Product product1 = new Product("과자", 1000);
+            Product product2 = new Product("아이스크림", 1000);
+            Product product3 = new Product("케이크", 1000);
+
+            // 속성(Property) 실습
+            Box box = new Box(10, 10);
+            box.Width = 100;
+            box.Height = 100;
+            box.GetArea();
+            try     // 잘못된 값이 들어간 경우
+            {
+                box.Width = -10;
+            } catch (Exception ex) {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
